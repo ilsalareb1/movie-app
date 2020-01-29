88b6ed7f1334b971377bbe6fcdfed49cad76b1b4
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2020-01-29 11:54:28 -0500

    Add files via upload

---
+++ review.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Review = mongoose.model("review", {
+  movieName: {
+    type: String,
+    required: true
+  },
+  reviewScore: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+    
+  },
+  reviewText: {
+    type: String,
+    required: true
+  },
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
+});
+
+module.exports = Review;
---
+++ reviews.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const Review = require("../models/review");
+const router = new express.Router();
+const auth = require("../middleware/auth");
+
+
+router.post("/reviews", auth,async (req, res) => {
+  const  review = new Review({
+    ...req.body,
+    owner: req.user._id
+  });
+    try {
+      await review.save();
+      res.send(review);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  router.get("/reviews", async (req, res) => {
+    try{
+      let reviews = await Review.find({});
+      res.send(reviews);
+    }catch (error){
+      res.status(500).send(error);
+    }
+  });
+  
+  router.get("/reviews/:id", async (req, res) => {
+    try{
+      let reviews = await Review.findById(req.params.id);
+      res.send(reviews);
+    }catch (error){
+      res.status(500).send(error);
+    }
+  });
+  
+  router.delete("/reviews/:id", auth, async (req, res)=>{
+    try{
+      const review = await Review.findByIdAndDelete(req.params.id);
+      res.send(review);
+    }catch{
+      res.status(500).send(error);
+    }
+  });
+  
+  router.patch("/reviews/:id", auth, async (req, res) => {
+    
+    const updates= Object.keys(req.body);
+    const allowedUpdates = ["reviewScore", "reviewText"];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update)
+    );
+    if (!isValidOperation) {
+      res
+        .status(400)
+        .send({ error: "Invalid Updates, please only update the review" });
+    }
+    try {
+      const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+      });
+      if (!review) {
+        return res.status(404).send();
+      }
+      res.send(review);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+ module.exports = router;
+  
